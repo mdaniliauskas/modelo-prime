4c5cce85125842b2ae8384d55d987cd0a96f66a4
Marcelo Daniliauskas <marcelo.daniliauskas@gmail.com>
2022-09-23 17:09:16 -0300

    Correcao - tema estava duplicado

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";                                //icons
 import "primeflex/primeflex.css";
-import "primereact/resources/themes/nova/theme.css"
 
  
 
